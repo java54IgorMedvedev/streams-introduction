5923b9f564d7886a262b8fa6f99c02500c2a9399
IgorMedvedev <psydov81@gmail.com>
2024-05-16 18:02:25 +0300

    cw 20

--- src/telran/streams/StreamIntroductionMethods.java
+++ src/telran/streams/StreamIntroductionMethods.java
@@ -1,34 +1,49 @@
 package telran.streams;
 
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class StreamIntroductionMethods {
-	static public int sumIf(int ar[], Predicate<Integer> predicate) {
-		return Arrays.stream(ar).filter(n -> predicate.test(n)).sum();
+static public int sumIf(int ar[], Predicate<Integer> predicate) {
+	return Arrays.stream(ar).filter(n -> predicate.test(n)).sum();
+}
+static public int sumDistinct(int ar[]) {
+	return Arrays.stream(ar).distinct().sum();
+}
+static public int maxIf(int ar[], Predicate<Integer> predicate) {
+	return Arrays.stream(ar).filter(n -> predicate.test(n)).max()
+			.orElseThrow(() -> new NoSuchElementException("empty stream"));
+}
+static public int[] sortDistinct(int ar[]) {
+	return Arrays.stream(ar).distinct().sorted().toArray();
+}
+static public void forEachIf(int ar[],
+		Predicate<Integer> ifPredicate, Consumer<Integer> forEachMethod) {
+	Arrays.stream(ar).filter(n -> ifPredicate.test(n))
+	.forEach(n -> forEachMethod.accept(n));
+}
+static public int[] getRandomArray(int fromInclusive, int toExclusive, int nNumbers) {
+	return new Random().ints(nNumbers, fromInclusive, toExclusive).toArray();
+}
+static public void displayShuffle(int ar[]) {
+	//prints out a given array in shuffled order
+	// example, array: [10, 20, 30,40], possible shuffling printing 30, 10, 40, 20
+	//no sorting
+	//no additional arrays, no any collections, no using standard shuffle method
+	//one code line
+	new Random().ints(0,ar.length).distinct().limit(ar.length)
+	.forEach(i -> System.out.print(ar[i] + " "));
+	
+}
+static public MinMaxAvg getMinMaxAvg( int[] ar ) {
+	if(ar.length == 0) {
+		throw new NoSuchElementException("Empty array");
 	}
-	static public int sumDistinct(int ar[]) {
-		return Arrays.stream(ar).distinct().sum();
-	}
-	static public int maxIf(int ar[], Predicate<Integer> predicate) {
-		return Arrays.stream(ar).filter(n -> predicate.test(n)).max()
-				.orElseThrow(() -> new NoSuchElementException("empty stream"));
-	}
-	static public int[] sortDistinct(int ar[]) {
-		return Arrays.stream(ar).distinct().sorted().toArray();
-	}
-	static public void forEachIf(int ar[], 
-			Predicate<Integer> ifPredicate, Consumer<Integer> forEachMethod) {
-		Arrays.stream(ar).filter(n -> ifPredicate.test(n))
-		.forEach(n -> forEachMethod.accept(n));
-	}
-	static public int[] getRandomArray(int fromInclusive, int toExclusive, int nNumbers) {
-		return new Random().ints(nNumbers, fromInclusive, toExclusive).toArray();
-	}
-	static public void displayShuffle(int ar[]) {
-	    new Random().ints(0, ar.length).distinct().limit(ar.length).map(i -> ar[i]).forEach(System.out::println);
-	}
+	IntSummaryStatistics stats = Arrays.stream(ar).summaryStatistics(); 
+	return new MinMaxAvg(stats.getMin(), stats.getMax(), stats.getAverage());
+}
 }
--- src/telran/streams/tests/StreamsIntroductionTests.java
+++ src/telran/streams/tests/StreamsIntroductionTests.java
@@ -3,56 +3,68 @@ package telran.streams.tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import telran.streams.MinMaxAvg;
+
 import static telran.streams.StreamIntroductionMethods.*;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 class StreamsIntroductionTests {
 
 	@Test
 	void sumIfTest() {
-		int[] ar = {1, 2, 3, 4, 5, 6};
+		int[] ar = {1,2,3,4,5,6};
 		assertEquals(12, sumIf(ar, n -> n % 2 == 0));
+		
 	}
 	@Test
 	void sumDistinctTest() {
-		int [] ar = {1, 2, 10, 1, 1};
+		int [] ar= {1, 2, 10, 1, 1};
 		assertEquals(13, sumDistinct(ar));
 	}
 	@Test
 	void maxIfTest() {
-		int [] ar = {1, 7, -3, 23, 13, 101};
+		int [] ar = {1, 7,-3, 23, 13, 101};
 		assertEquals(101, maxIf(ar, n -> n % 2 != 0));
 		assertThrowsExactly(NoSuchElementException.class,
 				() -> maxIf(ar, n -> n % 2 == 0));
 	}
 	@Test
 	void sortDistinctTest() {
-		int [] ar = {1, 10, 1, 1, 2};
+		int [] ar= {1, 10, 1, 1, 2};
 		int [] expected = {1, 2, 10};
 		assertArrayEquals(expected, sortDistinct(ar));
 	}
 	@Test
 	void forEachIfTest() {
-		int [] ar = {1, 10, 1, 1, 2};
+		int [] ar= {1, 10, 1, 1, 2};
 		forEachIf(ar, n -> n % 2 != 0, n -> System.out.print(n + " "));
 	}
 	@Test
-	void getRandomArrayTest() {
+	void  getRandomArrayTest() {
 		int ar[] = getRandomArray(10, 12, 1000);
 		assertEquals(1000, ar.length);
 		assertTrue(Arrays.stream(ar).allMatch(n -> n >= 10 && n < 12));
 		assertEquals(21, sumDistinct(ar));
 	}
 	@Test
-	void displayShuffleTest() {
-	    int[] ar = {10, 20, 30, 40};
-	    ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	    System.setOut(new PrintStream(outContent));
-	    displayShuffle(ar);
-	    assertTrue(outContent.toString().split("\n").length == 4);
-	    System.setOut(System.out);
+	void getMinMaxAvgTest() {
+		int[] ar = {1, 2, 3, 4, 5};
+		MinMaxAvg mma = getMinMaxAvg(ar);
+		assertEquals(1, mma.min());
+		assertEquals(5, mma.max());
+		assertEquals(3.0, mma.avg());
+		assertThrowsExactly(NoSuchElementException.class, () -> getMinMaxAvg(new int[0]));
 	}
+	@Test
+	void displayShuffleTest() {
+		int ar[] = {1, 2, 3, 4, 5, 6, 7};
+		for(int i = 0; i < 10; i++) {
+			System.out.println();
+			displayShuffle(ar);
+			
+		}
+	}
+
 }
